c80d467c4fa16442c0f1e2d187819173944c0b5e
Samuel Banning <samcbanning@gmail.com>
2022-08-05 18:50:25 -0400

    fixed

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 use bevy::prelude::*;
+use nokhwa;
 
 fn main() {
     App::new().run();
-    println!("this is a test")
+    println!("this is a test");
+
+    let mut camera = nokhwa::Camera::new(
+        0, // index
+        Some(nokhwa::CameraFormat::new_from(640, 480, nokhwa::FrameFormat::MJPEG, 30)), // format
+    )
+    .unwrap();
+    // open stream
+    camera.open_stream().unwrap();
+    loop {
+        let frame = camera.frame().unwrap();
+        println!("{}, {}", frame.width(), frame.height());
+    }
 }
