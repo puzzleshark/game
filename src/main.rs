6e64ce3131e03de4ad7c66bae63dff9e1a6842a0
Samuel Banning <samcbanning@gmail.com>
2022-08-05 20:16:59 -0400

    so easy!

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,58 @@
 use bevy::prelude::*;
-use nokhwa;
+// use nokhwa;
 
 fn main() {
-    App::new().run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
     println!("this is a test");
 
-    let mut camera = nokhwa::Camera::new(
-        0, // index
-        Some(nokhwa::CameraFormat::new_from(640, 480, nokhwa::FrameFormat::MJPEG, 30)), // format
-    )
-    .unwrap();
-    // open stream
-    camera.open_stream().unwrap();
-    loop {
-        let frame = camera.frame().unwrap();
-        println!("{}, {}", frame.width(), frame.height());
-    }
+    // let mut camera = nokhwa::Camera::new(
+    //     0, // index
+    //     Some(nokhwa::CameraFormat::new_from(640, 480, nokhwa::FrameFormat::MJPEG, 30)), // format
+    // )
+    // .unwrap();
+    // // open stream
+    // camera.open_stream().unwrap();
+    // loop {
+    //     let frame = camera.frame().unwrap();
+    //     println!("{}, {}", frame.width(), frame.height());
+    // }
+}
+
+/// set up a simple 3D scene
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // plane
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    });
+    // cube
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    });
+    // light
+    commands.spawn_bundle(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+    // camera
+    commands.spawn_bundle(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
 }
